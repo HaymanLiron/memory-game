fd5721e3d78be08a4910df35e84b67364d75c145
Liron <liron.hayman@gmail.com>
2016-06-29 12:57:05 +0300

    started to create the Board and Card classes in the Javascript

--- js/memory-game.js
+++ js/memory-game.js
@@ -2,28 +2,43 @@
  * Created by itc_user on 6/28/2016.
  */
 
-var NUM_CARDS = 12;
-
 function createElementWithClass(element, className){
     var newElement = document.createElement(element);
     newElement.className = className;
     return newElement;
 }
 
-function createElementWithId(element, idName){
-    var newElement = document.createElement(element);
-    newElement.id = idName;
-    return newElement;
+function Board() {
+    this.numCards = 12;
+    this.cards = [];
+    this.getCards = function(){
+        var output = [];
+        for (var i = 1; i <= this.numCards; i++){
+            console.log("I am here2");
+            var backImageSrc = "./images/texture.jpg";
+            var frontImageSrc = "./images/" + i + ".jpg";
+            //we push twice because we need two of each card!
+            for (var j = 0; j < 2; j++){
+                output.push(new Card(backImageSrc, frontImageSrc));
+            }
+        }
+        //TODO: shuffle cards before you add them to the HTML
+        return output;
+    };
+    this.cards = this.getCards();
+    this.currentlySelected = [];
 }
 
 function changeImage(clickEvent) {
-    clickEvent.target.style.backgroundColor = "white";
-    clickEvent.target.style.backgroundImage =  "url('https://s-media-cache-ak0.pinimg.com/avatars/sammy_dd_1424628161_140.jpg')";
+    console.log(clickEvent.target.backImage);
 }
 
-for (var i = 0; i < NUM_CARDS; i++) {
-    var card = createElementWithClass("div", "memory-card");
-    card.id = "card" + i;
-    card.addEventListener("click", changeImage);
-    document.body.appendChild(card);    
+function Card(backImage, frontImage) {
+    this.backImage = backImage;
+    this.frontImage = frontImage;
+    this.cardDiv = createElementWithClass("div", "memory-card");
+    this.cardDiv.addEventListener("click", changeImage);
+    document.body.appendChild(this.cardDiv);
 }
+
+var currentBoard = new Board();
