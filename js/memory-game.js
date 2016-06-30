a87be942cc421750d4a3784dc7e87faa4f71cc16
Liron <liron.hayman@gmail.com>
2016-06-30 11:27:10 +0300

    used bootstrap to center cards on a big screen refactored the handleClickOnCard function

--- js/memory-game.js
+++ js/memory-game.js
@@ -1,33 +1,49 @@
 var cardsCurrentlyTurned = [];
 var pairsFound = 0;
-var NUM_PAIRS = 12;
+var NUM_PAIRS = 4;
 var board = {
     cards: [
-        {id:"card1", front_src: "./images/1.jpg", back_src: "./images/texture.jpg"},
-        {id:"card2", front_src: "./images/2.jpg", back_src: "./images/texture.jpg"},
-        {id:"card3", front_src: "./images/3.jpg", back_src: "./images/texture.jpg"},
-        {id:"card4", front_src: "./images/4.jpg", back_src: "./images/texture.jpg"},
-        {id:"card5", front_src: "./images/5.jpg", back_src: "./images/texture.jpg"},
-        {id:"card6", front_src: "./images/6.jpg", back_src: "./images/texture.jpg"},
-        {id:"card7", front_src: "./images/7.jpg", back_src: "./images/texture.jpg"},
-        {id:"card8", front_src: "./images/8.jpg", back_src: "./images/texture.jpg"},
-        {id:"card9", front_src: "./images/9.jpg", back_src: "./images/texture.jpg"},
-        {id:"card10", front_src: "./images/10.jpg", back_src: "./images/texture.jpg"},
-        {id:"card11", front_src: "./images/11.jpg", back_src: "./images/texture.jpg"},
-        {id:"cardNUM_PAIRS", front_src: "./images/NUM_PAIRS.jpg", back_src: "./images/texture.jpg"}
+        {front_src: "./images/1.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/2.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/3.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/4.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/5.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/6.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/7.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/8.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/9.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/10.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/11.jpg", back_src: "./images/texture.jpg"},
+        {front_src: "./images/12.jpg", back_src: "./images/texture.jpg"}
     ]
 };
 
-function checkAllMatched(){
+function checkAllMatched() {
     if (pairsFound === NUM_PAIRS) {
         alert("Congratulations, you won!");
     }
 }
 
 
-function handleClickOnCard(clickEvent){
+function checkTwoCardsEqual(clickEvent) {
+    if (cardsCurrentlyTurned[0].getAttribute("front_src") === cardsCurrentlyTurned[1].getAttribute("front_src")) {
+        pairsFound++; //global variable
+        checkAllMatched();
+        cardsCurrentlyTurned = [];
+    } else { //if they don't match reset cardsCurrentlyTurned, and turn the cards back over after one second
+        setTimeout(function () {
+            for (var i = 0; i < cardsCurrentlyTurned.length; i++) {
+                cardsCurrentlyTurned[i].style.backgroundImage = "url(" + clickEvent.target.getAttribute("back_src") + ")";
+            }
+            cardsCurrentlyTurned = [];
+        }, 500);
+    }
+}
+
+function handleClickOnCard(clickEvent) {
     //flip the card over for a second by changing the background image
-    if (cardsCurrentlyTurned.length < 2 && clickEvent.target.style.backgroundImage === "url(\"" + clickEvent.target.getAttribute("back_src") + "\")"){
+
+    if (cardsCurrentlyTurned.length < 2 && clickEvent.target.style.backgroundImage === "url(\"" + clickEvent.target.getAttribute("back_src") + "\")") {
         //that if statement guarantees that a) won't show more than two cards, even if user can click quickly
         // b) doesn't do anything if the card was already flipped over
 
@@ -35,20 +51,9 @@ function handleClickOnCard(clickEvent){
         clickEvent.target.style.backgroundImage = "url(" + clickEvent.target.getAttribute("front_src") + ")";
         //add card to list of currently turned over
         cardsCurrentlyTurned.push(clickEvent.target);
-        if (cardsCurrentlyTurned.length === 2){ //2 cards flipped, let's see if they match
+        if (cardsCurrentlyTurned.length === 2) { //2 cards flipped, let's see if they match
             //if they match, increment the pairsFound count and reset the cards currently flipped
-            if (cardsCurrentlyTurned[0].getAttribute("front_src") === cardsCurrentlyTurned[1].getAttribute("front_src")){
-                pairsFound++; //global variable
-                checkAllMatched();
-                cardsCurrentlyTurned = [];
-            } else { //if they don't match reset the cards currently flipped, and turn the cards back over after one second
-                setTimeout(function(){
-                    for (var i = 0; i < cardsCurrentlyTurned.length; i++){
-                        cardsCurrentlyTurned[i].style.backgroundImage = "url(" + clickEvent.target.getAttribute("back_src") + ")";
-                    }
-                    cardsCurrentlyTurned = [];
-                }, 500);
-            }
+            checkTwoCardsEqual(clickEvent);
         }
     }
 }
@@ -58,10 +63,10 @@ function shuffleNumbers(n) {
     //where each number (0 through to (N-1)) appears exactly twice
     //agreed that it can be written WAY more efficiently
     var output = [];
-    while (output.length < n * 2){
-        var ranNum = Math.floor(Math.random()*NUM_PAIRS);
-        while (output.lastIndexOf(ranNum) !== -1 && output.lastIndexOf(ranNum) !== output.indexOf(ranNum) ){
-            ranNum = Math.floor(Math.random()*NUM_PAIRS);
+    while (output.length < n * 2) {
+        var ranNum = Math.floor(Math.random() * NUM_PAIRS);
+        while (output.lastIndexOf(ranNum) !== -1 && output.lastIndexOf(ranNum) !== output.indexOf(ranNum)) {
+            ranNum = Math.floor(Math.random() * NUM_PAIRS);
         }
         output.push(ranNum);
     }
@@ -69,15 +74,34 @@ function shuffleNumbers(n) {
 }
 
 var randOrder = shuffleNumbers(NUM_PAIRS);
+var maxCardsInEachRow = 4;
 
-for (var i = 0; i < randOrder.length; i++){
+for (var i = 0; i < randOrder.length; i++) {
+    //if first card in row, create space before card
+    if ((i + 1) % maxCardsInEachRow === 1){
+        var row = document.createElement("div");
+        row.classList.add("row");
+        var beginningDiv = document.createElement("div");
+        beginningDiv.classList.add("col-lg-4");
+        row.appendChild(beginningDiv);
+    }
+
     var div = document.createElement("div");
-    for (var keys in board['cards'][randOrder[i]]){
+    for (var keys in board['cards'][randOrder[i]]) {
         div.setAttribute(keys, board['cards'][randOrder[i]][keys]);
     }
     div.style.backgroundImage = "url(" + div.getAttribute("back_src") + ")";
     div.classList.add("memory-card");
+    div.classList.add("col-lg-1");
     div.addEventListener("click", handleClickOnCard);
-    document.body.appendChild(div);
+    row.appendChild(div);
+    //if we have reached enough cards in the row, add a break
+    if ((i + 1) % maxCardsInEachRow === 0){
+        var endDiv = document.createElement("div");
+        endDiv.classList.add("col-lg-4");
+        row.appendChild(endDiv);
+    }
+    document.body.appendChild(row);
 }
+
 
